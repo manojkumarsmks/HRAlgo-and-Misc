94a20514c0ba93095b0a8b7eb3ec9ca0a2aed336
Manoj Kumar Shivaswami <manojkumarsmks@gmail.com>
2018-04-09 00:58:02 -0600

    Sherlock and Square
    Comments

--- SherlockandSquares.java
+++ SherlockandSquares.java
@@ -1,7 +1,9 @@
+//https://www.hackerrank.com/challenges/sherlock-and-squares/problem
 import java.util.Scanner;
 
 public class SherlockandSquares {
 
+	//Number of integers between squareroot of a and b gives the result
 	static int squares(int a, int b) {
 		int sqrt_a = (int)Math.sqrt(a);
 		int sqrt_b = (int)Math.sqrt(b);
