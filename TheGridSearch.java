90ff503dfdf639438a136381351fbc3367f62b0f
manojkumarsmks <manojkumarsmks@gmail.com>
2018-04-30 02:26:18 -0600

    Finally ! fully working

--- TheGridSearch.java
+++ TheGridSearch.java
@@ -1,7 +1,6 @@
 // https://www.hackerrank.com/challenges/the-grid-search/problem
 import java.util.Scanner;
-
-public class TheGridSearch {
+ class TheGridSearch {
 
 	public static void main(String[] args) {
 		
@@ -20,61 +19,55 @@ public class TheGridSearch {
             for(int P_i = 0; P_i < r; P_i++){
                 P[P_i] = in.next();
             }
-            int iIndex = 0, jIndex = 0;
-            boolean flagResult = false;
             
-            while(P.length < (G.length - jIndex)) {
-            	String result = gridSearch(G, P[0], C, iIndex, jIndex);
-	            if(result == null) {
+            while(P.length <= G.length) {
+            	// Search for the first string in P in G
+            	// gridSearchchecks if the 1st string of P exists in G
+            	if(!gridSearch(G, P, C)) {
 	            	System.out.println("NO");
 	            	break;
 	            }
-	            String[] index = result.split(" ");
-	            iIndex = Integer.parseInt(index[0])+1;
-	            jIndex = Integer.parseInt(index[1]);
-	            if(checkForSubstring(G, P, Integer.parseInt(index[0]), Integer.parseInt(index[1]))) {
-	            	System.out.println("Check for substring true");
-	            	flagResult = true;
+	            else {
+	            	System.out.println("YES");
 	            	break;
 	            }
-	            else {
-	            	flagResult = false;
-	            }
             }
-            if(flagResult)
-            	System.out.println("YES");
-            else if(!flagResult)
-            	System.out.println("NO");
-            
         }
         in.close();
 
         
 	}
 
-	private static String gridSearch(String[] g, String p, int C, int iIndex, int jIndex) {
-		
-		for (int i = iIndex; i < g.length; i++) {
-			for(int j=jIndex; j<=C-p.length(); j++) {
-				if(g[i].substring(j, j+p.length()).equals(p))
-					return i+" " +j+" "+(j+p.length());
+	// return true if the P exists in G
+	private static boolean gridSearch(String[] g, String[] p, int C) {	
+		for (int i = 0; i < g.length; i++) {
+			System.out.println("GLEnghkl;laf "+(p[0].length()));
+			for(int j=0; j<=(g[i].length()-p[0].length()); j++) {
+				
+				String mainString = g[i].substring(j, j+ p[0].length());
+
+				String subString = p[0];
+					if(mainString.equals(subString)) {
+					if(checkForSubstring(g, p, i, j)) {
+						return true;
+					}
+				}
 			}
 		}
-		return null;
+		return false;
 	}
 
-	private static boolean checkForSubstring(String[] g, String[] p, int iIndex, int jIndex) {
-		boolean flag = true;
-		System.out.println("iIndex "+iIndex + " jIndex "+jIndex);
-		for(int i =0; i<p.length; i++) {
-			String mainString = g[iIndex].substring(jIndex, jIndex + p[i].length());
-			String subString = p[i];
-			if(!mainString.equals(subString)){
-				flag = false;
+	private static boolean checkForSubstring(String[] g, String[] p, int rowIndex, int columnIndex) {
+		int temp = p.length;
+		int pIndex = 0;
+		while(temp != 0) {
+			if(!g[rowIndex].substring(columnIndex, columnIndex+p[pIndex].length()).equals(p[pIndex])) {
+				return false;
 			}
-			iIndex++;
+			temp--;
+			rowIndex++;
+			pIndex++;
 		}
-		return flag;
+		return true;
 	}
-
 }
